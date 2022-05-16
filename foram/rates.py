b2c29e13bd87d1f8469fe4bcb90f082f77c2baef
Siham De Goeyse <64578907+sihamForam@users.noreply.github.com>
2022-05-16 12:10:05 +0200

    K+4_wrong
    gives the wrong value for  CO2 + HO - -> HCO3-

--- foram/rates.py
+++ foram/rates.py
@@ -7,3 +7,9 @@ def k_p1(t, s=35):
     a1 = (680.5 - 4.72 * s) * 1e8  # /s
     e1 = 69.4  # kJ / mol
     return a1 * np.exp(-e1 / (constants.R * (t + 273.15)))
+
+# def k_p4(t, s=35):
+       # """Rate equation for CO2 + HO- => HCO3- (Table 1) in kg /mol /s"""
+a4 = 8718 # kJ/mol
+e4 = 62.8 # kJ/mol
+K4 = a4 * np.exp(-e4/(constants.R * (t + 273.15)))
--- tests/test_rates.py
+++ tests/test_rates.py
@@ -6,4 +6,6 @@ def test_k_p1():
     assert np.isclose(np.round(foram.rates.k_p1(25, s=33.77), 3), 0.036)
 
 
-# test_k_p1()
+def test_k_p4():
+    assert np.isclose(np.round(foram.rates.k_p4(25, s=0), 3), 8500)
+
