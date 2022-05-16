3a1cb88013cad8c997a39393cbb78ac49a518ae4
bcala1 <105497348+bcala1@users.noreply.github.com>
2022-05-16 12:11:07 +0200

    tried to add k_m1 and K1 from Table 1

--- foram/rates.py
+++ foram/rates.py
@@ -1,4 +1,5 @@
 import numpy as np
+import PyCO2SYS as pyco2
 from . import constants
 
 
@@ -7,3 +8,22 @@ def k_p1(t, s=35):
     a1 = (680.5 - 4.72 * s) * 1e8  # /s
     e1 = 69.4  # kJ / mol
     return a1 * np.exp(-e1 / (constants.R * (t + 273.15)))
+
+def k_m1(t, s=35):
+    """Rate equation for HCO3 + H => CO2 + H2O (Table 1) in kg /mol /s"""
+   
+    return k_p1(t, s)/K1(t, s) 
+
+
+def K1(t, s=35):
+    """Equilibrium constant for CO2 + H2O => HCO3 + H"""
+    
+    #K1 = np.exp((-2307.1266/(t+273.15)) + 2.83655 - 1.5529413 * np.log(t+273.15) 
+               #  + ((-4.0484/(t+273.15)) - 0.20760841)* s**(1/2) + 0.08468345*s 
+              #  - 0.00654208 * s**(3/2) + np.log(1-0.001005 * s)) # mol kg 
+    
+    K1 = pyco2.k_carbonic_1(temperature=t, salinity=2, opt_k_carbonic=1)
+    
+    return K1
+
+K1(25)
--- tests/test_rates.py
+++ tests/test_rates.py
@@ -5,5 +5,15 @@ import foram
 def test_k_p1():
     assert np.isclose(np.round(foram.rates.k_p1(25, s=33.77), 3), 0.036)
 
+def test_k_m1():
+    assert np.isclose(np.round(foram.rates.k_m1(25, s=33.77), 1), 3.3e4)
 
-# test_k_p1()
+
+def test_K1():
+    assert np.isclose(np.round(foram.rates.K1(25, s=35), 4), -13.4847)
+
+foram.rates.K1(25, s=35)    
+#test_K1()   
+#test_k_m1()
+
+#foram.rates.k_m1(25, s=33.77)
