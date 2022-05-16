303254958727ae516fbf0b9e9c3ce64eef221f5a
ldenooijer <105633168+ldenooijer@users.noreply.github.com>
2022-05-16 12:10:43 +0200

    added and tested Kw and k_m4

--- foram/rates.py
+++ foram/rates.py
@@ -7,3 +7,11 @@ def k_p1(t, s=35):
     a1 = (680.5 - 4.72 * s) * 1e8  # /s
     e1 = 69.4  # kJ / mol
     return a1 * np.exp(-e1 / (constants.R * (t + 273.15)))
+
+def k_m4(t, s=35):
+    """Rate equation for CO2 + OH- -> HCO3- in /s"""
+    return Kw * ((k_m1*k_p4) / k_p1)
+
+def Kw (t, s=35):
+    """Equilibrium constant for H2O -> H+ + OH-"""
+    return np.exp(-13847.26/(t + 273.15) + 148.9652 - 23.6521 * np.log(t + 273.15) + (118.67/(t + 273.15) - 5.977 + 1.0495*np.log(t + 273.15))*s**0.5 - 0.01615 * s)
--- tests/test_rates.py
+++ tests/test_rates.py
@@ -7,3 +7,9 @@ def test_k_p1():
 
 
 # test_k_p1()
+
+def test_Kw():
+    assert np.isclose(np.round(np.log(foram.rates.Kw(25, s=35)), 3), -30.434)
+    
+def test_k_m4():
+    assert np.isclose(np.round(foram.rates.Kw(25, s=35), 3), 3.7*1e-4)
