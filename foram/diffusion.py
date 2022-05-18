c19cc2ca5c3ef328849cafe3fab10d74d7c68fb3
Matthew Humphreys <m.p.humphreys@icloud.com>
2022-05-18 09:12:20 +0200

    Put effective radii values in dict

--- foram/diffusion.py
+++ foram/diffusion.py
@@ -9,22 +9,17 @@ def mu(t, s):
     B = 1.827e-3
     C = 89.93
     mu_20 = 1.002e-3  # N s /m2
-
-    # not sure whether it is ln or log
+    # not sure whether it is ln or log10
     mu = np.exp((A * (20 - t) - B * (t - 20) ** 2) / (t + C)) * mu_20
-
     if s > 0:
-        """estimate viscosity of seawater, eq 14, based on data by Whitfield and Turner (1981)"""
+        # estimate viscosity of seawater, eq 14, based on Whitfield & Turner (1981)
         mu = mu / (0.9508 - 0.0007379 * t)
-
     return mu
 
 
-def Dc(t, a, s=35):  # values for a are in the constants
+def Dc(t, ion, s=35):  # values for a are in the constants
     """Diffusion, Stokes-Einstein relation), eq 11"""
-
-    Dc = (constants.kB * (t + 273.15)) / (6 * np.pi * mu(t, s) * a)
-
+    Dc = (constants.kB * (t + 273.15)) / (6 * np.pi * mu(t, s) * constants.radius[ion])
     return Dc
 
 
@@ -32,5 +27,4 @@ def Dc_CO2(t):
     """Diffusion coeff of CO2  by Jähne et al. (1987), eq 15"""
     a_co2 = 5019 * 1e-9  # m2 /s
     ea = 19.51  # kJ /mol
-
     return a_co2 * np.exp(-ea / (constants.R * (t + 273.15)))
