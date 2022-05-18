4708b07c4debc0911d617e6058bc4d4f8913482a
bcala1 <105497348+bcala1@users.noreply.github.com>
2022-05-18 20:32:37 +0200

    fixed the sigfig in the round function + defined absolute tolerance for isclose function

--- tests/test_diffusion.py
+++ tests/test_diffusion.py
@@ -4,37 +4,43 @@ import foram
 
 # Tests don't work because correct effective radii are not yet available
 
-
 def test_Dc_HCO3():
     assert np.isclose(
-        np.round(foram.diffusion.Dc(25, ion="HCO3", s=0), 2),
+        np.round(foram.diffusion.Dc(25, ion="HCO3", s=0), 11),
         1.18e-9,
+        atol=1e-11
     )
 
 
 def test_Dc_CO3():
     assert np.isclose(
-        np.round(foram.diffusion.Dc(25, ion="CO3", s=0), 3),
+        np.round(foram.diffusion.Dc(25, ion="CO3", s=0), 12),
         0.955e-9,
+        atol=1e-12
     )
 
 
 def test_Dc_H():
-    assert np.isclose(np.round(foram.diffusion.Dc(25, ion="H", s=0), 2), 9.31e-9)
+    assert np.isclose(
+        np.round(foram.diffusion.Dc(25, ion="H", s=0), 2), 
+        9.31e-9,
+        atol=1e-11
+    )
 
 
-# maybe these are also in 10^-9 but it is not clear from the paper and I don't know enough to say what is sensible
 def test_Dc_BOH3():
     assert np.isclose(
-        np.round(foram.diffusion.Dc(25, ion="BOH3", s=35), 2),
-        1.11,
+        np.round(foram.diffusion.Dc(25, ion="BOH3", s=35), 11),
+        1.11e-9,
+        atol=1e-11
     )
 
 
 def test_Dc_BOH4():
     assert np.isclose(
-        np.round(foram.diffusion.Dc(25, ion="BOH4", s=35), 2),
-        0.97,
+        np.round(foram.diffusion.Dc(25, ion="BOH4", s=35), 11),
+        0.97e-9,
+        atol=1e-11
     )
 
 
